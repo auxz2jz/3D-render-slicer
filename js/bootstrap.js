91685bfa7ce7570142adb6dd751af0b05b4b3cbb
auxz2jz <41704988+auxz2jz@users.noreply.github.com>
2026-09-06 22:52:04 -0700

    Load v1.0.1 tool usability refinements

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -29,7 +29,7 @@ async function loadClassic(src) {
 }
 
 try {
-  for (const src of ['./js/part01.js','./js/part02.js','./js/part03.js','./js/part04.js','./js/part05.js','./js/part06.js','./js/part07a.js','./js/part07b.js','./js/part08.js','./js/part09.js','./js/part10.js','./js/part11.js','./js/part12.js','./js/part13.js','./js/ui-shell.js']) {
+  for (const src of ['./js/part01.js','./js/part02.js','./js/part03.js','./js/part04.js','./js/part05.js','./js/part06.js','./js/part07a.js','./js/part07b.js','./js/part08.js','./js/part09.js','./js/part10.js','./js/part11.js','./js/part12.js','./js/part13.js','./js/part14.js','./js/ui-shell.js']) {
     await loadClassic(src);
   }
 } catch (error) {
